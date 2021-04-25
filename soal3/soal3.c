f11f914c8374282d49281cf727e149deb43a6620
Muhammad Farhan Haykal <70801807+neroSpace@users.noreply.github.com>
2021-04-25 18:57:22 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -12,6 +12,8 @@
 #include <fcntl.h>
 #include <errno.h>
 
+int sign;
+
 void daemonMode()
 {
 	pid_t pid, sid;
@@ -48,14 +50,26 @@ void caes_ciph(char* sukses, int shift){
         
     }
 }
+void changSIGN(){
+    sign = 0;
+}
 
 int main(int argc, char* argv[]){
-
     int stat;
-    FILE* kill;
-    kill = fopen("killer.sh", "w");
-    fprintf(kill, "#!/bin/bash\nkill %d\nkill %d\necho \'Killed program.\'\nrm \"$0\"", getpid() + 2, getpid() + 3);
-    fclose(kill);
+    if(argv[1][1]=='x'){
+        FILE* kill;
+        kill = fopen("killer.sh", "w");
+        fprintf(kill, "#!/bin/bash\nkillall -15 no3\necho \'Killed program.\'\nrm killer.sh");
+        fclose(kill);
+        signal(SIGTERM,changSIGN);
+    }
+    if(argv[1][1]=='z'){
+        FILE* kill;
+        kill = fopen("killer.sh", "w");
+        fprintf(kill, "#!/bin/bash\nkillall no3\necho \'Killed program.\'\nrm killer.sh");
+        fclose(kill);
+    }
+
     pid_t chillid;
     chillid = fork();
     if(chillid < 0) 
@@ -72,10 +86,9 @@ int main(int argc, char* argv[]){
 
     int stat_direct,stat_download,stat_zip,stat_enkripsi;
     char currenT1[50], currentT3[50], currenT2[50], link[50];
-    
-    while(1){
+    sign = 1;
+    while(sign){
         pid_t chillid1, chillid2, chillid3, chillid4, chillid5;
-        int x;
         time_t time1 = time(NULL);
         struct tm* loc1 = localtime(&time1);
         strftime(currenT1, 50, "%Y-%m-%d_%H:%M:%S", loc1);
@@ -85,64 +98,68 @@ int main(int argc, char* argv[]){
             exit(0);
         }
         if(chillid1==0){
-            char *direcargv[] = {"mkdir",currenT1, NULL};
-            execv("/bin/mkdir",direcargv);
-        }
-        
-        while(wait(&stat_direct)>0);
+            if(fork() == 0){
+            char *argv[] = {"mkdir",currenT1, NULL};
+            execv("/bin/mkdir",argv);
+            } else {
+                while(wait(&stat_direct)>0);
 
-        chillid2 = fork();
-        if(chillid2<0){
-            exit(0);
-        }
-        if(chillid2 == 0){
-            if(argv[1][1] == 'z') prctl(PR_SET_PDEATHSIG, SIGHUP);
-            chdir(currenT1);
-            for(x=1;x<=10;x++){
-                time_t time2 = time(NULL);
-                struct tm* loc2 = localtime(&time2);
-                
-                sprintf(link, "https://picsum.photos/%ld", (time2 % 1000) + 50);
-                strftime(currenT2, 50, "%Y-%m-%d_%H:%M:%S", loc2);
-                
-                chillid3 = fork();
-                if(chillid3 <0)
+                chillid2 = fork();
+                if(chillid2<0){
                     exit(0);
-                if(chillid3 == 0){
-                    char *linkargv[] = {"wget", link, "-O", currenT2, "-o", "/dev/null", NULL};
-                    execv("/usr/bin/wget",linkargv);
                 }
-                sleep(5);
+                if(chillid2 == 0){
+                    chdir(currenT1);
+                    for(int x=0;x<10;x++){
+                        time_t time2 = time(NULL);
+                        struct tm* loc2 = localtime(&time2);
+                    
+                        strftime(currenT2, 40, "%Y-%m-%d_%H:%M:%S", loc2);
+                        sprintf(link, "https://picsum.photos/%ld", (time2 % 1000) + 50);
+                        
+                        
+                        chillid3 = fork();
+                        if(chillid3 <0){
+                            exit(0);
+                        }
+                        if(chillid3 == 0){
+                            char *argv[] = {"wget", link, "-O", currenT2, "-o", "/dev/null", NULL};
+                            execv("/usr/bin/wget",argv);
+                        }
+                        sleep(5);
+                    }
+                    while(wait(&stat_download)>0);
+                    char sukses[30] = "Download Succes";
+                    caes_ciph(sukses, 5);
+                    FILE* encrypt = fopen("status.txt", "w");
+                    fprintf(encrypt,"%s",sukses);
+                    fclose(encrypt);
+
+                    while(wait(&stat_enkripsi)>0);
+                    chdir("..");
+                    strcpy(currentT3, currenT1);
+                    strcat(currentT3, ".zip");
+
+                    chillid4 = fork();
+                    if(chillid4 <0)
+                        exit(0);
+                    if(chillid4 == 0){
+                        char *argv[] = {"zip", "-r", currentT3, currenT1, NULL};
+                        execv("/usr/bin/zip", argv);
+                    }
+
+                    while(wait(&stat_zip)>0);
+                    chillid5 = fork();
+                    if(chillid5<0)
+                        exit(0);
+                    if(chillid5==0){
+                        char *argv[] = {"rm", "-r", currenT1, NULL};
+                        execv("/bin/rm", argv);
+                    }
+                } 
+                if (sign==0) break;
             }
-            while(wait(&stat_download)>0);
-            char sukses[30] = "Download Succes";
-            caes_ciph(sukses, 5);
-            FILE* encrypt = fopen("status.txt", "w");
-            fprintf(encrypt,"%s",sukses);
-            fclose(encrypt);
-            
         }
-            while(wait(&stat_enkripsi)>0);
-            chdir("..");
-            strcpy(currentT3, currenT1);
-            strcat(currentT3, ".zip");
-
-            chillid4 = fork();
-            if(chillid4 <0)
-                exit(0);
-            if(chillid4 == 0){
-                char *zippyargv[] = {"zip", "-r", currentT3, currenT1, NULL};
-                execv("/usr/bin/zip", zippyargv);
-            }
-
-            while(wait(&stat_zip)>0);
-            chillid5 = fork();
-            if(chillid5<0)
-                exit(0);
-            if(chillid5==0){
-                char *delargv[] = {"rm", "-r", currenT1, NULL};
-                execv("/bin/rm", delargv);
-            }
         sleep(40);
     }
 }
